bfb74734609e64e1856047b24767f70c39b5f4c6
manish263663 <manishchandraupreti8855@gmail.com>
2025-02-19 16:28:53 +0530

    datatypes

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,19 @@
-console.log("hello my name is manish")
+const accountId = 144553
+let accountEmail= "manish@gmail.com"
+var accountPassword ="123345"
+accountCity = "dehradun"
+
+// accountId =// 2 not allowed
+console.log(accountId);
+
+
+/*
+
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountEmail = "hc@gmail.com"
+accountPassword = "102389389798"
+accountCity = "ranikhet"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict "; //treat all Js code as newer version
+
+//alart(3+3) //we are use the nodejs not the browser
+
+console.log(3+
+    3)   // code readability shold ne high
+
+    console.log("manish")
+
+
+let name = "manish"
+let age = 18
+let isLoggedIn = false
+
+
+//number ==> 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+//undefined
+//symbol => unique
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined) ;//undefined because it is a type
