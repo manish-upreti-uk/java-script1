11c418c4704aca174aa0c2d62f4afc081d9a1b78
manish263663 <manishchandraupreti8855@gmail.com>
2025-02-20 10:26:15 +0530

    conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);  //nan type of value 
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+
+// let isLoggesIn = 1
+
+// let booleanIsLoggedIN = Boolean(isLoggesIn)
+// console.log(booleanIsLoggedIN);
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh => true"
+
+ let someNumber =44
+ let stringNumber = String(someNumber)
+
+console.log( stringNumber);
+
+console.log(typeof stringNumber);
