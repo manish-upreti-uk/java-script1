f94fb300a863e39f9b144dea86c15cea48104f26
manish263663 <manishchandraupreti8855@gmail.com>
2025-02-21 10:07:15 +0530

    new

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -23,9 +23,60 @@
 //"" => false
 //"hitesh => true"
 
- let someNumber =44
- let stringNumber = String(someNumber)
+//  let someNumber =44
+//  let stringNumber = String(someNumber)
 
-console.log( stringNumber);
+// console.log( stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//******************Operations******************
+//  */
+
+
+// let value =3 
+// let negValue = -value
+// //console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = "manish"
+// str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log("1" +"2") //12
+// console.log("1"+2+2);  // 122
+// console.log(1+2+"3"); //33
+// console.log((3+4)*5%5);
+
+// console.log(+true );
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1  = num2 =num3 =2+2
+
+// let gameCounter =100
+// gameCounter++;   //prefix and postfix 
+// console.log(gameCounter);
+
+// let x = 17
+// let y = ++x
+// console.log(x);
+// console.log(y);
+
+
+// const x1 = 17
+// const y1 = x++
+// console.log(x1);
+// console.log(y1);
+
+// let x = 3;
+// let y = ++x;
+// // x is 4; y is 3
+// console.log(x,y);
+
+// let x2 = 3n;
+// const y2 = x2++;
