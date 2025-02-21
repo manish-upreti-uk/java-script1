1325872e88e426c889fbdc545c1ba4d57305d6e2
manish263663 <manishchandraupreti8855@gmail.com>
2025-02-21 10:37:08 +0530

    datatypes

---
+++ dataTypes-summery.js
@@ -0,0 +1,43 @@
+//Primitive
+ //call by value
+
+// 7 types : string,Number, Boolean, null, undefined,Symbol,Bigint
+
+
+ //const score =100
+// const scoreValue = 100.3
+// const isLoggedIN = false
+// const outsideTemp =null
+// let userEmail ;
+
+// const id = Symbol('123')
+
+// const anotherid = Symbol('123')
+
+// console.log(id === anotherid);
+
+
+ const bigNumber =  728384882789932n
+
+//reference (nob primitive) //call by reference 
+
+// Array , objects, Function, 
+
+
+// const heros = ["shaktiman","naagraj","doga"];
+
+
+// let myObj =
+// {name:manish,
+//  age: 22,
+// }
+
+
+// const myfunction =function(){
+// console.log("hello");
+
+// }
+
+
+
+console.log(typeof bigNumber);
